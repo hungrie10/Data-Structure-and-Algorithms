7b50b056312880a8813cbbcf175c6e94eb6ed15e
Richard Mgbahurike <chocolatecookiepie387@gmail.com>
2026-01-22 09:20:39 +0100

    Auto save

--- Linked List/Linked_5.java
+++ Linked List/Linked_5.java
@@ -1,6 +1,6 @@
 public class Linked_5 {
 
-    public static class Node  {
+    public static class Node {
         int data;
         Node next;
 
@@ -9,6 +9,19 @@ public class Linked_5 {
             this.next = null;
         };
 
+    }
+    
+    public static Node InsertMyNode(){}
+    
+    public static void main(String[] args) {
+        Node headNode = new Node(50);
+        headNode.next = new Node(60);
+        headNode.next.next = new Node(60);
+
+
+
+
+         
     }
     
 }
