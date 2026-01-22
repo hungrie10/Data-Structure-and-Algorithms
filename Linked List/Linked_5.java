e181d5def4a675d0a65c359c646b30488fcaa43a
Richard Mgbahurike <chocolatecookiepie387@gmail.com>
2026-01-22 09:15:38 +0100

    Auto save

--- Linked List/Linked_5.java
+++ Linked List/Linked_5.java
@@ -1,1 +1,14 @@
-cl
+public class Linked_5 {
+
+    public static class Node  {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        };
+
+    }
+    
+}
---
+++ Linked List/autocommit.ps1
@@ -0,0 +1,5 @@
+while ($true) {
+  git add .
+  git commit -m "Auto save"
+  Start-Sleep -Seconds 300
+}
